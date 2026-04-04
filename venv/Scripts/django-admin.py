a4a27be81940884682fc9bc1012ca478c03da4cd
Shreedhar-D <shreedhardaingade@gmail.com>
2026-04-04 21:08:02 +0530

    project setup with migrations

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\shree\OneDrive\Desktop\DESK\FOSSEE\workshop_booking\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
